b3d7bc6ecbe40b23c5c228d6fdbd892d08bb2f4d
Teeraphat Kullanankanjana <ku.teeraphat_st@tni.ac.th>
2021-02-12 14:34:32 +0700

    Update socket_server.py
    -Add date time for specify server feedback
    -Add feedback handler if incoming massage isn't 1 or 2

--- socket_server.py
+++ socket_server.py
@@ -1,16 +1,17 @@
 """
 ----- socket_server.py-----
 Feedback incoming socket client request
-Version 0.1
+Version 0.2
 This project is a part of CPE-312 Embedded System
 Copyrights   Teeraphat Kullanankanjana
 """
 
 # import module
 import socket
+import datetime
 
 # host & port setup
-host = "169.254.148.106"
+host = "192.168.1.56"
 port = 1102
 
 # initializing server config
@@ -21,9 +22,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # protocol config
         s.listen(5)  # listen incoming massage
         while True:  # Infinity loop
             connection, address = s.accept()  # accept incoming client
-            print("Connected by {} ".format(address))
+            print("{} Connected by {} ".format(datetime.datetime.now(), address))
             with connection as c:  # received data handler
                 data = c.recv(1024)  # initializing received data variable
+                print("{} incoming massage from{} : {} \n".format(datetime.datetime.now(), address, data.decode("utf-8")))
                 c.sendall(b"Hello from server")  # feedback massage
                 # check the winning conditions
                 if data.decode("utf-8") == "1":
@@ -31,6 +33,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # protocol config
                 elif data.decode("utf-8") == "2":
                     print("Player 2 win")
                 else:
+                    print("Invalid data")
                     pass  # if received data not match then discard the data
     except OSError as e:
         print(e)  # print error to console
