3c2e960e37645f47d0c3c8798970407289e36c3a
Teeraphat Kullanankanjana <ku.teeraphat_st@tni.ac.th>
2021-02-12 18:55:37 +0700

    Update socket_server.py
    -Add sound effect when end game
    -Optimize date time to standard format

--- socket_server.py
+++ socket_server.py
@@ -1,39 +1,47 @@
 """
 ----- socket_server.py-----
 Feedback incoming socket client request
-Version 0.2
+Version 0.3
 This project is a part of CPE-312 Embedded System
 Copyrights   Teeraphat Kullanankanjana
 """
 
 # import module
 import socket
-import datetime
+import pygame
+from datetime import datetime
 
-# host & port setup
-host = "192.168.1.56"
+# setting host&port 
+host = "192.168.1.161"
 port = 1102
 
-# initializing server config
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # protocol config
-    try:  # error handle
-        s.bind((host, port))  # server start
-        print("Server start at IP {}:{}".format(host, port))
-        s.listen(5)  # listen incoming massage
-        while True:  # Infinity loop
-            connection, address = s.accept()  # accept incoming client
-            print("{} Connected by {} ".format(datetime.datetime.now(), address))
-            with connection as c:  # received data handler
-                data = c.recv(1024)  # initializing received data variable
-                print("{} incoming massage from{} : {} \n".format(datetime.datetime.now(), address, data.decode("utf-8")))
-                c.sendall(b"Hello from server")  # feedback massage
-                # check the winning conditions
-                if data.decode("utf-8") == "1":
-                    print("Player 1 win")
-                elif data.decode("utf-8") == "2":
-                    print("Player 2 win")
-                else:
-                    print("Invalid data")
-                    pass  # if received data not match then discard the data
-    except OSError as e:
-        print(e)  # print error to console
+pygame.init()
+now = datetime.now().strftime("%H:%M:%S")
+end_sound = pygame.mixer.Sound("/home/pi/Desktop/flukework/PythonSocket/victory.wav")
+def play_sound():
+    end_sound.play()
+# start socket server
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    try: # error handler 
+        s.bind((host,port)) # start server
+        print("{}: Server Start at IP {}:{}\nAwaiting Client Connection....\n".format(now,host,port)) # print server address 
+        s.listen(5)  # listen incoming message
+        while True: # connection lost handler 
+            connection, address = s.accept() # accept incoming client
+            print("\n{}: Connected by {} ".format(now,address)) # print client address
+            with connection as c: # sand data back to client  
+                data = c.recv(1024) # data recvied
+                c.sendall(b"Thank you for connecting from server") # send massage back to clinet
+                if data.decode("utf-8") == "1": # if recived data is 1 then player 1 win
+                    print("{}: Player 1 Lose\tPlayer 2 Win\n".format(now))
+                    play_sound()
+                elif data.decode("utf-8") == "2": # if recived data is 2 then player 2 win
+                    print("{}: Player 2 Lose\tPlayer 1 Win".format(now))
+                    play_sound()
+                else: # other recived data
+                    print("{} Invalid data ".format(now))
+                    pass # just pass recived data
+    except OSError as e: # print error 
+        print(e)
+
+
